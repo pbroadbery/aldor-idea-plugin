a40cd7f124ecf69420089a9bb3aaf6f572f606a8
Peter Broadbery <p.broadbery@gmail.com>
2017-03-12 23:39:18 +0000

    Disable very localised test

--- jps-plugin/src/test/java/aldor/builder/files/BuildFilesTest.java
+++ jps-plugin/src/test/java/aldor/builder/files/BuildFilesTest.java
@@ -1,21 +1,12 @@
 package aldor.builder.files;
 
-import org.junit.Test;
-
-import java.io.File;
-import java.util.Collections;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 public class BuildFilesTest {
-
-    @Test
+/*
     public void testAbsoluteNames() {
         File f = BuildFiles.buildDirectoryForFile(Collections.singletonList(new File("/home/pab/IdeaProjects/aldor-codebase")),
                                 new File("/home/pab/IdeaProjects/aldor-codebase/aldor/aldor/lib/comp/src/types/cinfer.as"));
         assertNotNull(f);
         assertEquals("/home/pab/IdeaProjects/aldor-codebase/build/lib/comp/src/types", f.getPath());
     }
-
+*/
 }
